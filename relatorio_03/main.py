696631f9f3b780df09d675bac44a72917b50f236
diogocastros1 <diogoantonio@gec.inatel.br>
2023-03-09 21:57:23 -0300

    Configuração de venv e conexão com BD via python

--- relatorio_03/main.py
+++ relatorio_03/main.py
@@ -6,5 +6,21 @@ db = Database(database="pokedex", collection="pokemons")
 tipos = ["Grass", "Poison"]
 pokemons = db.collection.find({ "type": {"$in": tipos}, "next_evolution": {"$exists": True} })
 
+fraquezas = ["Psychic", "Ice"]
+pokemons = db.collection.find({"weaknesses": {"$all": fraquezas}})
 
 writeAJson(pokemons, "pokemons")
+
+def getPokemonByName(name: str):
+    return db.collection.find({"name": name})
+
+pikachu = getPokemonByName("Pikachu")
+writeAJson(pikachu, "pikachu")
+
+def getPokemonsByType(types: list):
+    return db.collection.find({"type": {"$in": types}})
+
+types = ["Fighting"]
+pokemons = getPokemonsByType(types)
+
+writeAJson(pokemons, "pokemons_by_type")
