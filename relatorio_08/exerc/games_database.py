58891a97d945c1564095816c654aacaca08f4d80
diogocastros1 <diogoantonio@gec.inatel.br>
2023-05-12 20:51:21 -0300

    Relatorio 08

--- relatorio_08/exerc/games_database.py
+++ relatorio_08/exerc/games_database.py
@@ -13,47 +13,33 @@ class GamesDatabase:
         parameters = {"numero": numero}
         self.db.execute_query(query, parameters)
 
-    def create_relacionando_player(self, numero_partida, nome_player, pontos):
-        query = "MATCH (p:Player {name: $nome_player), (m:Match {numero_partida: $numero_partida}) CREATE (p)-[:PARTICIPOU{pontos: $pontos}]->(m)"
-        parameters = {"numero": numero_partida, "name": nome_player, "pontos":pontos}
+    def create_relacionando_player(self, numero_partida, player, pontos):
+        query = "MATCH (p:Player {name: $player}), (m:Match {numero: $numero_partida}) CREATE (p)-[:PARTICIPOU {pontos: $pontos}]->(m)"
+        parameters = {"numero_partida": numero_partida, "player": player, "pontos":pontos}
         self.db.execute_query(query, parameters)
 
     def get_players(self):
-        query = "MATCH (p:Players) RETURN p.name AS name"
+        query = "MATCH (p:Player) RETURN p.name AS name"
         results = self.db.execute_query(query)
         return [result["name"] for result in results]
 
     def get_match(self):
-        query = "MATCH (a:Match)<-[part:PARTICIPOU]-(p:Player) RETURN a.name, p.name, part.pontos"
+        query = "MATCH (a:Match)<-[part:PARTICIPOU]-(p:Player) RETURN a.numero, p.name, part.pontos"
         results = self.db.execute_query(query)
-        return [(result["a.name"], result["p.name"], result["part.pontos"]) for result in results]
+        return [(result["a.numero"], result["p.name"], result["part.pontos"]) for result in results]
+    
+    def get_match_espc(self, numero):
+        query = "MATCH (a:Match{numero:$numero})<-[part:PARTICIPOU]-(p:Player) RETURN p.name, part.pontos"
+        parameters = {"numero":numero}
+        results = self.db.execute_query(query, parameters)
+        return [(result["p.name"], result["part.pontos"]) for result in results]
 
-    def update_professor(self, old_name, new_name):
-        query = "MATCH (p:Professor {name: $old_name}) SET p.name = $new_name"
+    def update_player(self, old_name, new_name):
+        query = "MATCH (p:Player {name: $old_name}) SET p.name = $new_name"
         parameters = {"old_name": old_name, "new_name": new_name}
         self.db.execute_query(query, parameters)
-        
-    def insert_aluno_aula(self, aluno_name, aula_name):
-        query = "MATCH (a:Aluno {name: $aluno_name}) MATCH (b:Aula {name: $aula_name}) CREATE (a)-[:ASSISTE]->(b)"
-        parameters = {"aluno_name": aluno_name, "aula_name": aula_name}
-        self.db.execute_query(query, parameters)
-    
-    def insert_professor_aula(self, professor_name, aula_name):
-        query = "MATCH (a:Professor {name: $professor_name}) MATCH (b:Aula {name: $aula_name}) CREATE (a)-[:MINISTRA]->(b)"
-        parameters = {"professor_name": professor_name, "aula_name": aula_name}
-        self.db.execute_query(query, parameters)
 
-    def delete_professor(self, name):
-        query = "MATCH (p:Professor {name: $name}) DETACH DELETE p"
-        parameters = {"name": name}
-        self.db.execute_query(query, parameters)
-
-    def delete_aluno(self, name):
-        query = "MATCH (a:Aluno {name: $name}) DETACH DELETE a"
-        parameters = {"name": name}
-        self.db.execute_query(query, parameters)
-    
-    def delete_aula(self, name):
-        query = "MATCH (a:Aula {name: $name})<-[:MINISTRA]-(p:Professor) DETACH DELETE a"
+    def delete_player(self, name):
+        query = "MATCH (p:Player {name: $name}) DETACH DELETE p"
         parameters = {"name": name}
         self.db.execute_query(query, parameters)
--- relatorio_08/exerc/main.py
+++ relatorio_08/exerc/main.py
@@ -2,7 +2,7 @@ from database import Database
 from games_database import GamesDatabase
 
 # cria uma instância da classe Database, passando os dados de conexão com o banco de dados Neo4j
-db = Database("bolt://52.90.58.51:7687", "neo4j", "stoppering-million-perforation")
+db = Database("bolt://3.239.53.11:7687", "neo4j", "thing-flanges-rust")
 db.drop_all()
 
 # Instanciando a classe GamesDatabase
@@ -10,7 +10,7 @@ gd = GamesDatabase(db)
 
 # Criando alguns jogadores
 gd.create_player('Monica')
-gd.create_player('Joey')
+gd.create_player('Xandler')
 gd.create_player('Ross')
 gd.create_player('Rachel')
 
@@ -24,9 +24,22 @@ gd.create_relacionando_player(1,'Monica',7)
 gd.create_relacionando_player(1,'Rachel',9)
 gd.create_relacionando_player(2,'Rachel',10)
 gd.create_relacionando_player(2,'Ross',10)
-gd.create_relacionando_player(2,'Ross',7)
+gd.create_relacionando_player(3,'Ross',7)
 gd.create_relacionando_player(3,'Monica',10)
-gd.create_relacionando_player(3,'Joey',10)
+gd.create_relacionando_player(3,'Xandler',5)
+
+# Requisitando Informações
+print(f'Jogadores \n{gd.get_players()}\n')
+print(f'Numero da partida, Nome e Pontuação \n{gd.get_match()}\n')
+print(f'Competidores e pontuação \n{gd.get_match_espc(1)}\n')
+
+# Atualização do nome do jogador
+gd.update_player('Ross', 'Joey')
+print(f'Jogadores atualizados \n{gd.get_players()}\n')
+
+# Deletando jogador
+gd.delete_player('Xandler')
+print(f'Jogadores atualizados \n{gd.get_players()}\n')
 
 # Fechando a conexão com o banco de dados
 db.close()
